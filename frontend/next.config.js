0c7304d67ee119f9088ceb996d33a1ea10efdd8f
Nick Anderson <nickanderson@Nicks-MacBook-Pro.local>
2025-06-06 23:56:04 -0700

    shortURL

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: ['pickleyourspot.com'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'pickleyourspot.com'],
+    },
+  },
 }
 
 module.exports = nextConfig 
