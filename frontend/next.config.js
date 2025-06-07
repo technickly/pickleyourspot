e6e0ece4bdc723e12767b6dbf8d45f72b0408157
Nick Anderson <nickanderson@Nicks-MacBook-Pro.local>
2025-06-07 00:00:33 -0700

    shortURL

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,9 +10,13 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+        pathname: '/a/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
       },
     ],
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
     unoptimized: true,
   },
   experimental: {
