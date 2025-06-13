c08df08038a01b19819dbe3caa02a651507933fd
Nick Anderson <nickanderson@Nicks-MacBook-Pro.local>
2025-06-12 20:59:19 -0700

    emoji

--- frontend/app/components/PickleballIcon.tsx
+++ frontend/app/components/PickleballIcon.tsx
@@ -6,10 +6,11 @@ export default function PickleballIcon() {
       xmlns="http://www.w3.org/2000/svg"
       className="w-12 h-12 text-primary"
     >
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="currentColor"/>
-      <path d="M12 6c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm0 10c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z" fill="currentColor"/>
-      <path d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" fill="currentColor"/>
-      <path d="M19 12h-2v-2h2v2zm-14 0H3v-2h2v2z" fill="currentColor"/>
+      {/* Paddle */}
+      <path d="M19 8h-2V6c0-1.1-.9-2-2-2H9C7.9 4 7 4.9 7 6v2H5c-1.1 0-2 .9-2 2v4c0 1.1.9 2 2 2h2v2c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2v-2h2c1.1 0 2-.9 2-2v-4c0-1.1-.9-2-2-2z" fill="currentColor"/>
+      {/* Ball */}
+      <circle cx="12" cy="12" r="3" fill="white" stroke="currentColor" strokeWidth="1"/>
+      <circle cx="12" cy="12" r="1.5" fill="currentColor"/>
     </svg>
   );
 } 
